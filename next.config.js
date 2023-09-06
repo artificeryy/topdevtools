99811bfd2d92d94046bf1f2bec7ea63024b0c2ff
artificeryy <artificery88@gmail.com>
2023-09-06 22:01:55 +0800

    [MAINT] Add rewrites to next.config to direct to fontchroma

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const { MAIN_URL } = "topdev.tools"
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: `/:path*`,
+            },
+            {
+                source: '/fontchroma',
+                destination: `${MAIN_URL}/fontchroma`,
+            },
+            {
+                source: '/fontchroma/:path*',
+                destination: `${MAIN_URL}/fontchroma/:path*`,
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
