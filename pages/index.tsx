2f640c3723089a52f9fc703d02609a4a75512def
artificeryy <artificery88@gmail.com>
2023-09-07 12:18:48 +0800

    [MAINT] Update page title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import MainNav from "@/components/main-nav"
 import Hero from "@/components/hero"
 import Tools from "@/components/tools"
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={`${inter.className}`}>
+      <Head>
+        <title>TopDev.tools</title>
+      </Head>
       <MainNav />
       <Hero />
       <Tools />
